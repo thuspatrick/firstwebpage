3f37547079deccd19c20b4512c95ddd0999c00f3
Patrick T <52385783+thuspatrick@users.noreply.github.com>
2019-08-13 22:59:26 +0100

    config et menu scss

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+
+let ShowMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!ShowMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(Item => Item.classList.add("show"));
+
+    ShowMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(Item => Item.classList.remove("show"));
+  }
+}
